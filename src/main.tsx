9ba748eca3485170ff9b9042a1b9bb3952cfa445
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-09 09:55:02 -0300

    chore: organize project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 /** biome-ignore-all lint/style/noNonNullAssertion: react rules */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import './index.css'
-import App from './App.tsx'
+import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
