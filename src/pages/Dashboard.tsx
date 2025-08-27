69000789b7248d17d2610a314a6e21a30379304e
PedroJGC <pedro.juliogc02@gmail.com>
2025-08-27 17:20:50 -0300

    feat: pagination component

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import searchSvg from '../assets/search.svg'
 import { Button } from '../components/Button'
 import { Input } from '../components/Input'
+import { Pagination } from '../components/Pagination'
 import { RefundItem } from '../components/RefundItem'
 import { CATEGORIES } from '../utils/categories'
 import { formatCurrency } from '../utils/formatCurrency'
@@ -45,6 +46,8 @@ export function Dashboard() {
       <div className="flex flex-col gap-4 mt-6 max-h-[342px] overflow-y-scroll">
         <RefundItem data={REFUND_EXAMPLE} />
       </div>
+
+      <Pagination current={1} total={10} />
     </div>
   )
 }
